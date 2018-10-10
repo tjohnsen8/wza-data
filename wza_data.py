448746b4863486744bc9e83950bcda21cb0a1a53
tom.johnsen <tom.johnsen@edgetech.com>
2018-10-10 14:19:50 -0400

    loop through the 4 main divisions to get data and save json and csv for each

--- wza_data.py
+++ wza_data.py
@@ -2,10 +2,11 @@ import requests
 import json
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
 get_data = 0
-write_csv = 0
-
+show_data = 1
 '''
 Response
 dict_keys(['AffiliateFilterValues', 'DisplayNation', 'Workouts', 
@@ -25,6 +26,48 @@ key name : {... 'Res': result ...}
 '''
 
 workout_key_list = ['workout_5605', 'workout_5606', 'workout_5607']
+csv_headers = ['Name', 'WOD 1', 'WOD 2', 'WOD 3']
+workout_key_map = {'workout_5605' : 'WOD 1', 
+				   'workout_5606' : 'WOD 2', 
+				   'workout_5607' : 'WOD 3'}
+wza_divison_names = ['elite_men', 'elite_women', 'int_men', 'int_women']
+wza_division_urls = {'elite_men': 'https://qualifier.wodapalooza.com/events/1244/results/scoring-group/male_5031/json?', 
+					 'elite_women': 'https://qualifier.wodapalooza.com/events/1244/results/scoring-group/female_5809/json?', 
+					 'int_men': 'https://qualifier.wodapalooza.com/events/1244/results/scoring-group/male_5032/json?', 
+					 'int_women' : 'https://qualifier.wodapalooza.com/events/1244/results/scoring-group/female_5810/json?'}
+
+def plot_workout_1(wod1, division):
+	wod1.hist(grid=True, bins='auto', rwidth=0.9, color='#607c8e')
+	plt.text(1000, 80, wod1.describe())
+	def fmtsec(x,pos):
+		return "{:02d}:{:02d}".format(int(x//60), int(x%60))
+	plt.gca().xaxis.set_major_formatter(mticker.FuncFormatter(fmtsec))
+	# Use nice tick positions as multiples of 30 seconds
+	plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(60))
+	plt.xlabel('time')
+	plt.ylabel('frequency')
+	plt.title('wza wod1 {}'.format(division))
+	plt.show()
+
+def plot_workout_2(wod2, division):
+	wod2.hist(grid=True, bins=48, rwidth=0.9, color='#607c8e')
+	plt.text(33, 50, wod2.describe())
+	#wod3.hist(grid=True, bins=20, rwidth=0.9, color='#607c8e')
+	#plt.text(300, 80, wod3.describe())
+	plt.xlabel('reps')
+	plt.ylabel('frequency')
+	plt.title('wza wod2 {}'.format(division))
+	#wod3.hist(bins=50)
+	plt.show()
+
+def plot_workout_3(wod3, division):
+	wod3.hist(grid=True, bins=20, rwidth=0.9, color='#607c8e')
+	plt.text(300, 80, wod3.describe())
+	plt.xlabel('weight')
+	plt.ylabel('frequency')
+	plt.title('wza wod3 {}'.format(division))
+	#wod3.hist(bins=50)
+	plt.show()
 
 def get_workout_result(ath_scores):
 	workout_result = {}
@@ -47,37 +90,54 @@ def get_workout_result(ath_scores):
 		elif workout_name == 'workout_5607':
 			# weight
 			res = float(workout_json['Res'].split('<span>')[0])
-		workout_result[workout_name] = res
+		workout_result[workout_key_map[workout_name]] = res
 	return workout_result
 
-def get_data_from_leaderboard():
+def get_data_from_leaderboard(division, url):
 	# get updated data from website
-	response = requests.get('https://qualifier.wodapalooza.com/events/1244/results/scoring-group/male_5032/json?')
+	response = requests.get(url)
 	json_data = response.json()
-	with open('wzadata.json', 'w') as outfile:
+	with open('{}.json'.format(division), 'w') as outfile:
 		json.dump(json_data, outfile)
 	return json_data
 
 def get_data_from_file():
-	
 	with open('wzadata.json', 'r') as infile:
 		json_data = json.load(infile)
 	return json_data
 
-if __name__ == '__main__':
-	if get_data:
-		wzajson = get_data_from_leaderboard()
-	else:
-		wzajson = get_data_from_file()
-
-	if write_csv:
-		with open('wzascores.csv', 'w', newline='') as csvfile:
-			writer = csv.DictWriter(csvfile, fieldnames=workout_key_list)
+def write_data_to_csv(division, athletes):
+	with open('{}.csv'.format(division), 'w', newline='', encoding='utf-8') as csvfile:
+			writer = csv.DictWriter(csvfile, fieldnames=csv_headers)
 			writer.writeheader()
-			for athlete in wzajson['Athletes']:
+			for athlete in athletes:
 				ath_scores = get_workout_result(athlete['WorkoutScores'])
+				ath_scores['Name'] = athlete['Name']
 				writer.writerow(ath_scores)
 
-	# open csv for shit
-	wza_data = pd.read_csv('wzascores.csv', sep=',', header=0)
-	print(wza_data.values)
+def get_all_results():
+	# loop through the divisions and get the results
+	for division, url in wza_division_urls.items():
+		json_data = get_data_from_leaderboard(division, url)
+		write_data_to_csv(division, json_data['Athletes'])
+
+
+if __name__ == '__main__':
+	
+	if get_data:
+		get_all_results()
+	
+	# viz data
+	if show_data:
+		for division in wza_divison_names:
+			wza_data = pd.read_csv('{}.csv'.format(division), sep=',', header=0)
+			# remove 0 scores
+			wod1 = wza_data.loc[:,csv_headers[1]]
+			wod2 = wza_data.loc[:,csv_headers[2]]
+			wod3 = wza_data.loc[:,csv_headers[3]]
+			wod1 = wod1[wod1 > 0]
+			wod2 = wod2[wod2 > 0]
+			wod3 = wod3[wod3 > 0]
+			plot_workout_1(wod1, division)
+			plot_workout_2(wod2, division)
+			plot_workout_3(wod3, division)
